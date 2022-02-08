fe096490cc15de76489663707615beb8a7c490c7
DungSherlock <dungsherlock@gmail.com>
2022-02-08 08:55:50 +0700

    --allow-empty

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cat /tmp/amz | htmlq --text '#outOfStock'
+curl -s 'https://www.amazon.com/Ninja-BL710WM-Professional-Countertop-Technology/dp/B08JQR8RWS' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62' | grep "priceAmount.*"
