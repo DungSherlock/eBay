875707d7a961ff7bedba5da0ce9905a2719cff06
DungSherlock <dungsherlock@gmail.com>
2022-02-15 19:51:22 +0700

    --allow-empty

--- sed.sh
+++ sed.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sed -i -e 's/"curl/curl/g' track.sh
-
-# find2=`done\n"`
-# find3=`done\n`
-# sed -i -e 's/$find2/$find3/g' track.sh
-sed -i -E 's/done\r"/done\n/g' track.sh
+sed -i -e 's/"while/while/g' track.sh
+sed -i -E 's/fi"/fi/g' track.sh
 sed -i -e 's/""/"/g' track.sh
+./track.sh
