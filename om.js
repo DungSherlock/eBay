d2726c2c35ec7cc12a4a2044daf45aa537319309
DungSherlock <dungsherlock@gmail.com>
2022-03-12 10:29:43 +0700

    --allow-empty

---
+++ om.js
@@ -0,0 +1,8 @@
+let response = fetch("https://www.amazon.com/dp/B078YQTD2H");
+fetch(url)
+    .then(response => {
+        // handle the response
+    })
+    .catch(error => {
+        // handle the error
+    });
