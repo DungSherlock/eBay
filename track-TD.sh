117f217ae7d5d243bc0622681bfbc9481f4000fe
DungSherlock <dungsherlock@gmail.com>
2022-05-07 20:14:18 +0700

    --allow-empty

--- track-TD.sh
+++ track-TD.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-: > track-usItemId-CO.txt
-: > track-priceValue-CO.txt
-: > track-fullName-CO.txt
-: > track-address-CO.txt
-: > track-status-CO.txt
-: > track-number-CO.txt
-: > track-url-CO.txt
-: > track-OrderID-CO.txt
-: > track-email-CO.txt
-: > track-categoriesType-CO.txt
+: > track-usItemId-TD.txt
+: > track-priceValue-TD.txt
+: > track-fullName-TD.txt
+: > track-address-TD.txt
+: > track-status-TD.txt
+: > track-number-TD.txt
+: > track-url-TD.txt
+: > track-OrderID-TD.txt
+: > track-email-TD.txt
+: > track-categoriesType-TD.txt
 
 
 while read line
@@ -21,7 +21,7 @@ do
       -H 'sec-ch-ua: " Not;A Brand";v="99", "Microsoft Edge";v="97", "Chromium";v="97"' \
       -H 'x-o-platform: rweb' \
       -H 'dnt: 1' \
-      -H 'x-o-correlation-id: pn9BnlEx0zDiyvRGJ6nL5BS9pDN7erB017Nf' \
+      -H 'x-o-TDrrelation-id: pn9BnlEx0zDiyvRGJ6nL5BS9pDN7erB017Nf' \
       -H 'device_profile_ref_id: E75qSsFMPUgfUnNtpgnTkFKD0ikbbLGTuMXJ' \
       -H 'x-latency-trace: 1' \
       -H 'wm_mp: true' \
@@ -48,10 +48,6 @@ do
       --data-raw $'{"query":"query getGuestOrder($orderId:ID\u0021 $emailAddress:String\u0021){guestOrder(input:{id:$orderId emailAddress:$emailAddress}){...OrderFieldsFragment}}fragment cancelReason on OrderCancelReason{__typename subReasonCode subDescription}fragment priceDetailFragment on OrderPriceDetailRow{label value displayValue info{title message}rowInfo{...orderPriceDetailRowInfo}}fragment orderPriceDetailRowInfo on OrderPriceDetailRowInfo{title message{...textFragment}}fragment price on Price{displayValue value}fragment variants on MapEntry{name value}fragment OrderAddOn on OrderAddOn{lineId uniqueLineId productInfo{name usItemId offerId}quantityString quantityLabel type fulfillmentInstructions{...textFragment}actions{manageProtectionPlan{...orderActionFragment}cancel}priceInfo{linePrice{...price}}quantity isActive}fragment orderDonationDetails on OrderDonationDetails{message{...textFragment}emailReceipt emailReceiptToken status{...textFragment}errorStatus}fragment orderLineItem on OrderLineItem{id uniqueId actions{contactSeller cancel addToCart configureCake reviewItem resendEGiftCardToken protectionPlan{...orderActionFragment}manageProtectionPlan{...orderActionFragment}}isGift digitalDeliveryMessage quantity quantityString quantityLabel isSubstitutionSelected fulfilledItems{id quantityString priceInfo{itemPrice{value}}productInfo{usItemId name}}isReturnable returnEligibilityMessage productInfo{name usItemId imageInfo{thumbnailUrl}canonicalUrl offerId orderLimit orderMinLimit weightIncrement salesUnit salesUnitType isSubstitutionEligible isAlcohol}reshop{reshopMessage isShippingAvailable minPromiseDate maxPromiseDate}selectedVariants{...variants}variantAdditionalInfo{parts{text nativeAction}}priceInfo{priceDisplayCodes{showItemPrice priceDisplayCondition finalCostByWeight}itemPrice{...price}linePrice{...price}unitPrice{...price}preDiscountedLinePrice{...price}additionalLines{name value}}discounts{label labelText{...textFragment}}itemReviewed activationCodes{label code}protectionPlanMessage{...textFragment}showSeller isShippedByWalmart seller{id name isPro}digitalDeliveryPhoneNumber addOns{...OrderAddOn}multiboxBundleId}fragment DriverFragment on Driver{id firstName photoUrl}fragment OrderGroupFragment on OrderGroup{driver{...DriverFragment}deliveryDate fulfillmentType status{...OrderGroupStatusFragment}showSeller isShippedByWalmart seller{...SellerFragment}id itemCount items{...LiteLineItemFragment}pickupPerson{firstName lastName email}accessPointId shipment{...ShipmentFragment}returnEligibilityMessage actions{...OrderGroupActionsFragment}recipientEmailAddress digitalDelivery{...DigitalDeliveryFragment}tireInstallationReservation{status extraText reservationId}}fragment OrderGroupStatusFragment on OrderGroupStatus{statusType showStatusTracker statusTracker{status label isCurrent}message{...textFragment}subtext subMessage{...textFragment}notice helpCenterText{...textFragment}}fragment textFragment on Text{parts{bold url text nativeAction lineBreak}}fragment LiteLineItemFragment on OrderLineItem{id quantity productInfo{name usItemId imageInfo{thumbnailUrl}offerId isAlcohol}priceInfo{linePrice{...price}}}fragment SellerFragment on GroupSeller{id name isPro}fragment DigitalDeliveryFragment on DigitalDelivery{title name instructions{...textFragment}}fragment ShipmentFragment on Shipment{id trackingNumber isExternalTracking trackingUrl proofOfDelivery{...ProofOfDeliveryFragment}}fragment OrderGroupActionsFragment on OrderGroupActions{reorder edit track changeSlot checkin editDeliveryInstructions editPickupPerson editTip tip rateDriver cancel enableTip{...orderActionFragment}enableEdit{...orderActionFragment}rescheduleTireInstall{...orderActionFragment}cancelTireInstall{...orderActionFragment}help viewCancellationDetails{...cancellationDetailsActionFragment}}fragment cancellationDetailsActionFragment on CancellationDetailsAction{label heading text{...textFragment}}fragment ProofOfDeliveryFragment on ProofOfDelivery{photoUrl photoPreviewUrl showPreview}fragment DeliveryInstructionsFragment on DeliveryInstructions{text type typeText}fragment GiftDetailsFragment on GiftDetails{recipientAddress{fullName}recipientEmail senderName giftMessage}fragment SubstitutionAction on SubstitutionAction{message label type}fragment ODPGroupCategoryFragment on OrderCategory{type name subtext returnMessage substitutionsBanner{...textFragment}showExtendedSubstitutions actions{substitutions{...SubstitutionAction}nilPickReshop{message action{text url}}returnDetails viewCancellationDetails{...cancellationDetailsActionFragment}trackOnInHomeApp}banner{...textFragment}accordionState items{...orderLineItem}substitutions{...orderLineItem fulfilledItems{...orderLineItem}}returnInfo{...returnInfoFragment}}fragment ODPGroupFragment on OrderGroup{id fulfillmentType deliveryMessage deliveryAddress{fullName firstName lastName address{...addressFragment}}deliveryInstructions{...DeliveryInstructionsFragment}deliveryPreferences{text{...textFragment}cta{...orderActionFragment}}editSubstitutionsCutOff status{statusType showStatusTracker message{...textFragment}notice helpCenterText{...textFragment}}itemCount isCategorized categories{...ODPGroupCategoryFragment}seller{id name isPro}shipment{id trackingNumber}actions{reorder edit track changeSlot checkin editDeliveryInstructions editPickupPerson tip cancel help enableTip{...orderActionFragment}enableEdit{...orderActionFragment}viewCancellationDetails{...cancellationDetailsActionFragment}enableInHome{...orderActionFragment}createGiftReceipt}cutOffTimestamp isEditSubstitutionsEligible isInHome giftDetails{...GiftDetailsFragment}donationDetails{...orderDonationDetails}}fragment returnInfoFragment on ReturnInfo{returnOrderId type refundPriceDetails{__typename...refundPriceDetailsFragment}refundMessage tierRefundMessage paymentMethods{__typename description cardType paymentType displayValues message{...textFragment}}actions{__typename generate scheduleOrModifyPickup{text url}reschedulePickup editPickupContact}shippingLabelUrl qrCodeImageUrl carriers{__typename id name}pickupCarrier pickupContact{nameAndAddress{fullName firstName lastName address{addressString city state postalCode addressLineOne addressLineTwo country}}phone}pickupConfirmationNumber bannerMessage{...textFragment}}fragment refundPriceDetailsFragment on RefundPriceDetails{subTotal{__typename...orderPriceDetailRowFragment}fees{__typename...orderPriceDetailRowFragment}discounts{__typename...orderPriceDetailRowFragment}taxTotal{__typename...orderPriceDetailRowFragment}grandTotal{__typename...orderPriceDetailRowFragment}}fragment orderPriceDetailRowFragment on OrderPriceDetailRow{label displayValue value info{__typename title message}}fragment addressFragment on OrderAddress{addressString addressLineOne addressLineTwo state postalCode city}fragment ODPPickupInfo on OrderGroup{pickupInstructions pickupPerson{...person}alternatePickupPerson{...person}store{id name address{...addressFragment}}}fragment person on OrderPickupPerson{firstName lastName email}fragment ODPTippingInfo on OrderGroup{addTipMessage{...textFragment}driver{...DriverFragment}tipping{min{...price}max{...price}suggested{...price}preselected}subtotal{...price}}fragment orderActionFragment on Action{text url}fragment orderCustomer on OrderCustomer{id firstName lastName email isGuest isEmailRegistered}fragment OrderFieldsFragment on Order{__typename id version type customer{...orderCustomer}displayId idBarcodeImageUrl(barWidth:3 barHeight:100) isFuelPurchase title shortTitle timezone tippableGroup{...OrderGroupFragment}amendableGroup{id changeSlotIterationsLeft cutOffTimestamp fulfillmentType deliveryAddress{fullName firstName lastName address{addressString addressLineOne addressLineTwo state postalCode city country}}isAmendInProgress}substitutionsBanner{heading subheading longSubheading}groups_2101{__typename...OrderGroupFragment...ODPGroupFragment...ODPPickupInfo...ODPTippingInfo}multiboxBundles{...orderLineItem}itemCancelReasons{__typename...cancelReason}groupCancelReasons{__typename...cancelReason}priceDetails{__typename subTotal{__typename...priceDetailFragment}taxTotal{__typename...priceDetailFragment}grandTotal{__typename...priceDetailFragment}authorizationAmount{__typename...priceDetailFragment}fees{__typename...priceDetailFragment}discounts{__typename...priceDetailFragment}minimumThreshold{__typename...price}belowMinimumFee{__typename...priceDetailFragment}driverTip{__typename...priceDetailFragment}donations{__typename...priceDetailFragment}}paymentMethods{__typename description cardType paymentType displayValues message{...textFragment}actions{connectToCapitalOne{...orderActionFragment}visitAffirm{...orderActionFragment}}}actions{__typename return pendingReturn cancel startReturn{...orderActionFragment}reorder}banners{...textFragment}}","variables":{"orderId":"'${array[2]}'","emailAddress":"'${array[1]}'"}}' \
       --compressed`
       
-      # echo $curl
-      # echo $curl | jq '.data.guestOrder.groups_2101[].items[].productInfo.usItemId'
-      # echo '"'${array[4]}'"'
-      # sleep 10
 
       if [[ $curl =~ "blocked" ]]; then
         link=`echo $curl | jq '.redirectUrl' | sed 's/"//g'`
@@ -64,56 +60,56 @@ do
 
     if [[ $curl =~ "error" ]]; then
         echo "1-Sai info"
-        echo ${array[2]} >> track-OrderID-CO.txt
-        echo ${array[1]} >> track-email-CO.txt
-        echo "Sai info" >> track-usItemId-CO.txt
-        echo "Sai info" >> track-priceValue-CO.txt
-        echo "Sai info" >> track-fullName-CO.txt
-        echo "Sai info" >> track-address-CO.txt
-        echo "Sai info" >> track-status-CO.txt
-        echo "Sai info" >> track-number-CO.txt
-        echo "Sai info" >> track-url-CO.txt
-        echo "Sai info" >> track-categoriesType-CO.txt
+        echo ${array[2]} >> track-OrderID-TD.txt
+        echo ${array[1]} >> track-email-TD.txt
+        echo "Sai info" >> track-usItemId-TD.txt
+        echo "Sai info" >> track-priceValue-TD.txt
+        echo "Sai info" >> track-fullName-TD.txt
+        echo "Sai info" >> track-address-TD.txt
+        echo "Sai info" >> track-status-TD.txt
+        echo "Sai info" >> track-number-TD.txt
+        echo "Sai info" >> track-url-TD.txt
+        echo "Sai info" >> track-categoriesType-TD.txt
     else
-        sum=`md5 track-OrderID-CO.txt`
+        sum=`md5 track-OrderID-TD.txt`
         for (( i=0; i<=4; i++ ))
         do
             for (( j=0; j<=4; j++))
             do
                 if [[ `echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].items['${j[@]}'].productInfo.usItemId'` == '"'${array[3]}'"' ]]; then
-                    echo ${array[2]} >> track-OrderID-CO.txt
-                    echo ADOT220427042407372 >> track-email-CO.txt
-                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].items['${j[@]}'].productInfo.usItemId' >> track-usItemId-CO.txt
-                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].items['${j[@]}'].priceInfo.linePrice.value' >> track-priceValue-CO.txt
-                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].deliveryAddress.fullName' >> track-fullName-CO.txt
-                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].deliveryAddress.address.addressString' >> track-address-CO.txt
-                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].status.message.parts[].text' >> track-status-CO.txt
-                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].shipment.trackingNumber' | sed 's/null//g' >> track-number-CO.txt
-                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].shipment.trackingUrl' | sed 's/null//g' >> track-url-CO.txt
-                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].categories[].type' >> track-categoriesType-CO.txt
+                    echo ${array[2]} >> track-OrderID-TD.txt
+                    echo ADOT220427042407372 >> track-email-TD.txt
+                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].items['${j[@]}'].productInfo.usItemId' >> track-usItemId-TD.txt
+                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].items['${j[@]}'].priceInfo.linePrice.value' >> track-priceValue-TD.txt
+                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].deliveryAddress.fullName' >> track-fullName-TD.txt
+                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].deliveryAddress.address.addressString' >> track-address-TD.txt
+                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].status.message.parts[].text' >> track-status-TD.txt
+                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].shipment.trackingNumber' | sed 's/null//g' >> track-number-TD.txt
+                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].shipment.trackingUrl' | sed 's/null//g' >> track-url-TD.txt
+                    echo $curl | jq '.data.guestOrder.groups_2101['${i[@]}'].categories[].type' >> track-categoriesType-TD.txt
                     break
                 fi
             done
         done
-        if [[ $sum == `md5 track-OrderID-CO.txt` ]]; then
+        if [[ $sum == `md5 track-OrderID-TD.txt` ]]; then
             echo ${array[0]}"-Khác Item"
-            echo "'"${array[2]} >> track-OrderID-CO.txt
-            echo ${array[1]} >> track-email-CO.txt
-            echo "Khác Item" >> track-usItemId-CO.txt
-            echo "Khác Item" >> track-priceValue-CO.txt
-            echo "Khác Item" >> track-fullName-CO.txt
-            echo "Khác Item" >> track-address-CO.txt
-            echo "Khác Item" >> track-status-CO.txt
-            echo "Khác Item" >> track-number-CO.txt
-            echo "Khác Item" >> track-url-CO.txt
-            echo "Khác Item" >> track-categoriesType-CO.txt
+            echo "'"${array[2]} >> track-OrderID-TD.txt
+            echo ${array[1]} >> track-email-TD.txt
+            echo "Khác Item" >> track-usItemId-TD.txt
+            echo "Khác Item" >> track-priceValue-TD.txt
+            echo "Khác Item" >> track-fullName-TD.txt
+            echo "Khác Item" >> track-address-TD.txt
+            echo "Khác Item" >> track-status-TD.txt
+            echo "Khác Item" >> track-number-TD.txt
+            echo "Khác Item" >> track-url-TD.txt
+            echo "Khác Item" >> track-categoriesType-TD.txt
         else
             echo ${array[0]}
         fi   
 
 fi
 
-done < input-wm-track-CO.txt
+done < input-wm-track-TD.txt
 
 git add -A .
 git commit -m --allow-empty
@@ -122,5 +118,5 @@ git push origin HEAD -f
 gitCommit=`git rev-parse HEAD`
 linkGit=`echo https://raw.githubusercontent.com/DungSherlock/eBay/`$gitCommit`echo /`
 linkApi=`echo https://script.google.com/macros/s/AKfycbxvgB0cMGKRjx_sVCDTrrzTkT40YIeRPsBUp-1-e913NLyI8mUywjiaGDu9MDj9bChN/exec?`
-linkPost=$linkApi`echo Item ID==IMPORTDATA\(\"`$linkGit`echo track-usItemId-CO.txt\"\)\&Giá==IMPORTDATA\(\"`$linkGit`echo track-priceValue-CO.txt\"\)\&Tên==IMPORTDATA\(\"`$linkGit`echo track-fullName-CO.txt\"\)\&Địa chỉ==index\(IMPORTDATA\(\"`$linkGit`echo track-address-CO.txt\"\),,1\)\&Status==IMPORTDATA\(\"`$linkGit`echo track-status-CO.txt\"\)\&Tracking Number==IMPORTDATA\(\"`$linkGit`echo track-number-CO.txt\"\)\&Tracking URL==IMPORTDATA\(\"`$linkGit`echo track-url-CO.txt\"\)\&Email==IMPORTDATA\(\"`$linkGit`echo track-email-CO.txt\"\)\&Order==IMPORTDATA\(\"`$linkGit`echo track-OrderID-CO.txt\"\)\&categoriesType==IMPORTDATA\(\"`$linkGit`echo track-categoriesType-CO.txt\"\)`
+linkPost=$linkApi`echo Item ID==IMPORTDATA\(\"`$linkGit`echo track-usItemId-TD.txt\"\)\&Giá==IMPORTDATA\(\"`$linkGit`echo track-priceValue-TD.txt\"\)\&Tên==IMPORTDATA\(\"`$linkGit`echo track-fullName-TD.txt\"\)\&Địa chỉ==index\(IMPORTDATA\(\"`$linkGit`echo track-address-TD.txt\"\),,1\)\&Status==IMPORTDATA\(\"`$linkGit`echo track-status-TD.txt\"\)\&Tracking Number==IMPORTDATA\(\"`$linkGit`echo track-number-TD.txt\"\)\&Tracking URL==IMPORTDATA\(\"`$linkGit`echo track-url-TD.txt\"\)\&Email==IMPORTDATA\(\"`$linkGit`echo track-email-TD.txt\"\)\&Order==IMPORTDATA\(\"`$linkGit`echo track-OrderID-TD.txt\"\)\&categoriesType==IMPORTDATA\(\"`$linkGit`echo track-categoriesType-TD.txt\"\)`
 echo $linkPost
