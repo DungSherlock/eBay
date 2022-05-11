54e5c6df6c3be24a8bb1b7430f3ffd8045cf9ee9
DungSherlock <dungsherlock@gmail.com>
2022-05-11 22:04:25 +0700

    --allow-empty

--- test.sh
+++ test.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
-link=`https://script.google.com/macros/s/AKfycbxvgB0cMGKRjx_sVCDTrrzTkT40YIeRPsBUp-1-e913NLyI8mUywjiaGDu9MDj9bChN/exec\?Item ID==IMPORTDATA("https://raw.githubusercontent.com/DungSherlock/eBay/785379a43e75acbb4bdc1f44371d29233c62414f/track-usItemId-CO.txt")&Giá==IMPORTDATA("https://raw.githubusercontent.com/DungSherlock/eBay/785379a43e75acbb4bdc1f44371d29233c62414f/track-priceValue-CO.txt")\&Tên==IMPORTDATA("https://raw.githubusercontent.com/DungSherlock/eBay/785379a43e75acbb4bdc1f44371d29233c62414f/track-fullName-CO.txt")\&Địa chỉ==index(IMPORTDATA("https://raw.githubusercontent.com/DungSherlock/eBay/785379a43e75acbb4bdc1f44371d29233c62414f/track-address-CO.txt"),,1)\&Status==IMPORTDATA("https://raw.githubusercontent.com/DungSherlock/eBay/785379a43e75acbb4bdc1f44371d29233c62414f/track-status-CO.txt")\&Tracking Number==IMPORTDATA("https://raw.githubusercontent.com/DungSherlock/eBay/785379a43e75acbb4bdc1f44371d29233c62414f/track-number-CO.txt")\&Tracking URL==IMPORTDATA("https://raw.githubusercontent.com/DungSherlock/eBay/785379a43e75acbb4bdc1f44371d29233c62414f/track-url-CO.txt")\&Email==IMPORTDATA("https://raw.githubusercontent.com/DungSherlock/eBay/785379a43e75acbb4bdc1f44371d29233c62414f/track-email-CO.txt")\&Order==IMPORTDATA("https://raw.githubusercontent.com/DungSherlock/eBay/785379a43e75acbb4bdc1f44371d29233c62414f/track-OrderID-CO.txt")\&categoriesType==IMPORTDATA("https://raw.githubusercontent.com/DungSherlock/eBay/785379a43e75acbb4bdc1f44371d29233c62414f/track-categoriesType-CO.txt")`
-echo $link | 
+: > ebay-usItemId.txt
+: > ebay-currentPrice.txt
+: > ebay-wasPrice.txt
+: > ebay-availabilityStatus.txt
+
+while read line
+do
+  array=(${line//|/ })
+
+  while true
+  do
+    list=`echo blocked`
+    if [[ $list = "blocked" ]]
+    then
+        link=`echo $list | jq '.redirectUrl' | sed 's/"//g'`
+    #   `open -n -a /Applications/Microsoft\ Edge.app --args --profile-directory=Default https://walmart.com$link`
+        echo "Tạm dừng!" ${array[0]}
+        echo ${line}
+        read -p "blocked"
+    else
+      echo ${array[0]}
+      break
+    fi
+  done
+
+done < input-wm-list.txt
