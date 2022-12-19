a5d831e5a071a8e420161d5e416667966a93860a
HueDung <dungsherlock@gmail.com>
2022-12-19 06:50:30 +0000

    --allow-empty

--- giayEbay.sh
+++ giayEbay.sh
@@ -16,7 +16,7 @@
 while read line
 do
   echo http://www.ebay.com/itm/$line
-  nguon+=`curl http://www.ebay.com/itm/$line \
+  nguon+=`curl 'http://www.ebay.com/itm/'$line' \
   -H 'authority: www.ebay.com' \
   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
   -H 'accept-language: vi' \
