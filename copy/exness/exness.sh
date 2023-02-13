4efa4a5b95daa03f89bf8b1cd02bf2174caa5b78
DungSherlock <dungsherlock@gmail.com>
2023-02-13 17:50:06 +0700

    --allow-empty

--- copy/exness/exness.sh
+++ copy/exness/exness.sh
@@ -9,7 +9,7 @@
   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54' \
   --compressed | jq '.total'`
     result=$(echo "$total / 34" | bc)
-    echo $result
+    echo $total
 
 
 for i in $(seq 1 $result)
@@ -19,7 +19,17 @@ do
   -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41' \
   --compressed`
     echo "page $i/$result"
-    echo $exness | jq '.result[].account' | sed 's/+//g' >> ROI7.txt
+    echo $exness | jq '.result[].account' >> account.txt
+    echo $exness | jq '.result[].name' >> name.txt
+    echo $exness | jq '.result[].trading_time' >> trading_time.txt
+    echo $exness | jq '.result[].leverage' >> leverage.txt
+    echo $exness | jq '.result[].risk_score' >> risk_score.txt
+    echo $exness | jq '.result[].commission' >> commission.txt
+    echo $exness | jq '.result[].profitability' >> profitability.txt
+    echo $exness | jq '.result[].drawdown' >> drawdown.txt
+    echo $exness | jq '.result[].investors' >> investors.txt
+    echo $exness | jq '.result[].equity' >> equity.txt
+    echo $exness | jq '.result[].trading_period' >> trading_period.txt
 done
 
 git add -A .
