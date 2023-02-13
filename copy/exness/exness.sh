3754c27516f61a65f7ec98620593286ccffcda81
DungSherlock <dungsherlock@gmail.com>
2023-02-14 00:13:26 +0700

    --allow-empty

--- copy/exness/exness.sh
+++ copy/exness/exness.sh
@@ -49,7 +49,7 @@ git push
 git push origin HEAD -f
 gitCommit=`git rev-parse HEAD`
 linkGit=`echo https://raw.githubusercontent.com/DungSherlock/eBay/`$gitCommit`echo /copy/bybit/`
-linkApi=`echo https://script.google.com/macros/s/AKfycbzqe1xUYUS3DMYILrf4gi0r3jlf5rok46Yvx_2BSVEu_p_oVj5hdv3q-YXoMUG-N7HB/exec?`
+linkApi=`echo https://script.google.com/macros/s/AKfycby6z7gCg6QHCMx6iiGA7Ioc3IR7y7k0G5B_NgZWSWQpcEmI6iSUBAM0e0VQkyyVoe3KSQ/exec?`
 linkPost=$linkApi`echo account==IMPORTDATA\(\"`$linkGit`echo account.txt\"\)\
 \&name==IMPORTDATA\(\"`$linkGit`echo name.txt\"\)\
 \&trading_time==IMPORTDATA\(\"`$linkGit`echo trading_time.txt\"\)\
