534efcbddcf86f10eda4ea1d5cbdbdc0a7574707
DungSherlock <dungsherlock@gmail.com>
2023-03-03 13:15:18 +0700

    --allow-empty

--- track-Hiep.sh
+++ track-Hiep.sh
@@ -130,7 +130,7 @@ linkPost=$linkApi`echo Item ID==IMPORTDATA\(\"`$linkGit`echo track-Hiep-usItemId
 \&categoriesType==IMPORTDATA\(\"`$linkGit`echo track-Hiep-categoriesType-CO.txt\"\)\
 `
 
-echo $linkGit
+echo $linkPost
 
 # echo '<?xml version="1.0" encoding="UTF-8"?>
 # <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
