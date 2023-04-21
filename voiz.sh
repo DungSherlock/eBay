e5aef0b7bab68f39f5e06b54c8ce56189a95ff8b
DungSherlock <dungsherlock@gmail.com>
2023-04-21 14:07:19 +0700

    --allow-empty

--- voiz.sh
+++ voiz.sh
@@ -4,6 +4,8 @@ read -p 'Nhap ID: ' idBook
 book=`curl -s https://api.voiz.vn/v1/playlists/$idBook`
 # title=`echo $book | jq '.data.author_string + "-" + .data.name' | sed 's/"//g' | sed 's/ /%20/g' | sed 's/-/%20D/g'`
 title=`echo $book | jq '.data.author_string + "-" + .data.name' | sed 's/"//g'`
+echo $title | sed 's/%/%25/g; s/ /%20/g; s/!/%21/g; s/"/%22/g; s/#/%23/g; s/\$/%24/g; s/\&/%26/g; s/'\''/%27/g; s/(/%28/g; s/)/%29/g; s/\*/%2A/g; s/+/%2B/g; s/,/%2C/g; s/-/%2D/g; s/\./%2E/g; s/\//%2F/g; s/:/%3A/g; s/;/%3B/g; s//%3C/g; s/=/%3D/g; s//%3E/g; s/?/%3F/g; s/@/%40/g; s/\[/%5B/g; s/\\/%5C/g; s/\]/%5D/g; s/\^/%5E/g; s/_/%5F/g; s/`/%60/g; s/{/%7B/g; s/|/%7C/g; s/}/%7D/g; s/~/%%7E/g'
+
 list=`curl -s https://api.voiz.vn/v1/playlists/$idBook/audios\?limit\=1000\&order\=asc\&position\=bottom`
 echo $list | jq '.data[].name' > voiz-name.txt
 echo $list | jq '.data[].id' > voiz-id.txt
