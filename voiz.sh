cdfeb7a6ee0666123a11594bdf5ded9a55acbf35
DungSherlock <dungsherlock@gmail.com>
2023-04-21 13:30:08 +0700

    --allow-empty

--- voiz.sh
+++ voiz.sh
@@ -8,25 +8,18 @@ echo $list | jq '.data[].name' > voiz-name.txt
 echo $list | jq '.data[].id' > voiz-id.txt
 
 
-linkPost=`echo \
-\title=`$title`\
+git add -A .
+git commit -m --allow-empty
+git push
+git push origin HEAD -f
+gitCommit=`git rev-parse HEAD`
+linkGit=`echo https://raw.githubusercontent.com/DungSherlock/eBay/`$gitCommit`echo /`
+linkApi=`echo https://script.google.com/macros/s/AKfycbyX2_giAjG24Gh0HmVsErUD2r1ikuoUjkfhMDHfMsOopjy7OcimmUpTXPB87JmnSikwVw/exec?`
+linkPost=$linkApi`echo \
+\title=`$title` echo \
 \&id==IMPORTDATA\(\"`$linkGit`echo voiz-id.txt\"\)\
 \&name==IMPORTDATA\(\"`$linkGit`echo voiz-name.txt\"\)\
 `
-echo $linkPost
-
-# git add -A .
-# git commit -m --allow-empty
-# git push
-# git push origin HEAD -f
-# gitCommit=`git rev-parse HEAD`
-# linkGit=`echo https://raw.githubusercontent.com/DungSherlock/eBay/`$gitCommit`echo /`
-# linkApi=`echo https://script.google.com/macros/s/AKfycbyX2_giAjG24Gh0HmVsErUD2r1ikuoUjkfhMDHfMsOopjy7OcimmUpTXPB87JmnSikwVw/exec?`
-# linkPost=$linkApi`echo \
-# \title=`$title`\
-# \&id==IMPORTDATA\(\"`$linkGit`echo voiz-id.txt\"\)\
-# \&name==IMPORTDATA\(\"`$linkGit`echo voiz-name.txt\"\)\
-# `
-# echo '[InternetShortcut]\
-# URL='$linkPost > voiz.url
-# open voiz.url
+echo '[InternetShortcut]\
+URL='$linkPost > voiz.url
+open voiz.url
