ec192cbe6fd7d688946b63cd7832cde18729e3a3
DungSherlock <dungsherlock@gmail.com>
2023-04-23 07:43:42 +0700

    --allow-empty

--- voiz.sh
+++ voiz.sh
@@ -5,8 +5,16 @@
 
 read -p 'Nhap ID: ' idBook
 book=`curl -s https://api.voiz.vn/v1/playlists/$idBook`
-echo $book | jq '.data.author_string + "-" + .data.name' > voiz-title.txt
+folder=`echo $book | jq '.data.author_string + "-" + .data.name'` | sed 's/^[[:space:]]*//' | sed 's/[[:space:]]*$//' > voiz-title.txt
+echo $folder
 echo $idBook >> voiz-title.txt
+mkdir "/Users/om/Downloads/voiz/$folder"
+image=`echo $book | jq '.data.avatar.original_url'`
+
+echo $image
+exit
+
+curl -H -LOs "Host: voiz-prod.s3.cloud.cmctelecom.vn" -H "Accept: image/x-ms-bmp,image/x-win-bitmap,image/bmp,image/tiff,image/ico,image/x-icon,image/jpeg,image/x-bmp,image/x-xbitmap,image/gif,image/png" -H "Accept-Language: en-US;q=1.0, vi-US;q=0.9" -H "User-Agent: wewe/12.0.0 (com.wewe.musicsounds; build:20230223.161211; iOS 16.4.1) Alamofire/12.0.0" --compressed $image
 
 
 list=`curl -s https://api.voiz.vn/v1/playlists/$idBook/audios\?limit\=1000\&order\=asc\&position\=bottom`
