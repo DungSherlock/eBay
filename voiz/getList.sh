3c9d2500b143bd4cc114be711e1b506236aadaf5
DungSherlock <dungsherlock@gmail.com>
2023-04-26 09:39:09 +0700

    --allow-empty

--- voiz/getList.sh
+++ voiz/getList.sh
@@ -12,7 +12,7 @@ do
     outputName=`echo $line | cut -d '|' -f5`
     while true
     do
-        curl -C - -LOs -H "Host: voiz-prod.s3.cloud.cmctelecom.vn" -H "Accept-Language: vi-VN,vi;q=0.9" -H "Accept: */*" -H "User-Agent: AppleCoreMedia/1.0.0.20E252 (iPhone; U; CPU OS 16_4_1 like Mac OS X; vi_vn)" -H "Referer: stream.voiz.app" --compressed "$m3u8Link"
+        curl -C - -Ls -H "Host: voiz-prod.s3.cloud.cmctelecom.vn" -H "Accept-Language: vi-VN,vi;q=0.9" -H "Accept: */*" -H "User-Agent: AppleCoreMedia/1.0.0.20E252 (iPhone; U; CPU OS 16_4_1 like Mac OS X; vi_vn)" -H "Referer: stream.voiz.app" --compressed "$m3u8Link" --output "$idAudio-$outputName.m3u8"
         if [ $? -eq 0 ]
         then
             break
@@ -20,7 +20,7 @@ do
             echo "Có lỗi xảy ra, chờ 3s"
             sleep 3
     done
-    cp "$outputName.m3u8" "$name.sh"
+    cp "$idAudio-$outputName.m3u8" "$name.sh"
     gsed -i '/^#/d' "$name.sh"
     text1='while true; do curl -C - -LOs -H "Host: voiz-prod.s3.cloud.cmctelecom.vn" -H "Accept-Language: en-US,en;q=0.9" -H "Accept: */*" -H "User-Agent: AppleCoreMedia/1.0.0.20E252 (iPhone; U; CPU OS 16_4_1 like Mac OS X; en_us)" -H "Referer: stream.voiz.app" "'
     text2='https://voiz-prod.s3.cloud.cmctelecom.vn/uploads/audio/filename/'$idLink'/'
@@ -30,7 +30,5 @@ do
     lastRow='cat part*'$outputName'*.ts > '$outputName'.ts && ffmpeg -i '$outputName'.ts -map 0:a:0 -c copy -y "../$(basename -s .sh "$0").aac" && rm *"'$outputName'"*""'
     gsed -i "1s/^/$firstRow/" "$name.sh"
     echo $lastRow >> "$name.sh"
-    mv "$outputName.m3u8" ""
 
 done < list.txt
-# rm getList.sh
