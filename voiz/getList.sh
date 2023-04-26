9ef4491143767c0aad111e06871dca14819cc7b8
DungSherlock <dungsherlock@gmail.com>
2023-04-26 08:01:10 +0700

    --allow-empty

--- voiz/getList.sh
+++ voiz/getList.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-curl -C - -Ls https://script.google.com/macros/s/AKfycbwUXljrnjCBVZRFbDIbzNtmv9eFrSD-ejpOzXqvwXv2XO6Ybb1JtK5o1Xv9_ffr2QIySw/exec | jq '.content.names[]' | sed 's/"//g' > m3u8/list.txt
-
+# curl -C - -Ls https://script.google.com/macros/s/AKfycbwUXljrnjCBVZRFbDIbzNtmv9eFrSD-ejpOzXqvwXv2XO6Ybb1JtK5o1Xv9_ffr2QIySw/exec | jq '.content.names[]' | sed 's/"//g' > list.txt
+code list.txt
+echo "Press any key to continue..."
+read -n 1 -s -r # wait for a single key press
 while read line
 do
     idAudio=`echo $line | cut -d '|' -f1`
@@ -11,7 +13,8 @@ do
     while true
     do
         curl -C - -LOs -H "Host: voiz-prod.s3.cloud.cmctelecom.vn" -H "Accept-Language: vi-VN,vi;q=0.9" -H "Accept: */*" -H "User-Agent: AppleCoreMedia/1.0.0.20E252 (iPhone; U; CPU OS 16_4_1 like Mac OS X; vi_vn)" -H "Referer: stream.voiz.app" --compressed "$m3u8Link"
-        if [ $? -eq 0 ] then
+        if [ $? -eq 0 ]
+        then
             break
         fi
             echo "Có lỗi xảy ra, chờ 3s"
@@ -24,10 +27,10 @@ do
     text3='"; if [ $? -eq 0 ]; then break; fi; echo "Có lỗi xảy ra, chờ 3s"; sleep 3; done'
     gsed -i "s/^/${text1//\//\\/}${text2//\//\\/}/g; s/$/${text3//\//\\/}/g" "$name.sh"
     firstRow="#!\/bin\/bash\n"
-    lastRow='cat part*'$outputName'*.ts > '$outputName'.ts && ffmpeg -i '$outputName'.ts -map 0:a:0 -c copy -y "$(basename -s .sh "$0").aac" && rm *"'$outputName'"*""'
+    lastRow='cat part*'$outputName'*.ts > '$outputName'.ts && ffmpeg -i '$outputName'.ts -map 0:a:0 -c copy -y "../$(basename -s .sh "$0").aac" && rm *"'$outputName'"*""'
     gsed -i "1s/^/$firstRow/" "$name.sh"
     echo $lastRow >> "$name.sh"
-    mv "$outputName.m3u8" "m3u8/"
+    mv "$outputName.m3u8" ""
 
-done < m3u8/list.txt
+done < list.txt
 # rm getList.sh
